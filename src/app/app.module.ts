3e97535b9c187acc620975b48912b687a33ddc0d
oussamafaleh <oussamafaleh1998@gmail.com>
2020-09-07 13:15:45 +0100

    ngfy portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './sections/home/home.component';
+import { AboutComponent } from './sections/about/about.component';
+import { ExperienceComponent } from './sections/experience/experience.component';
+import { WorkComponent } from './sections/work/work.component';
+import { EducationComponent } from './sections/education/education.component';
+import { BlogComponent } from './sections/blog/blog.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ExperienceComponent,
+    WorkComponent,
+    EducationComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
